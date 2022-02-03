bbc713fe061c80a48d9ccac809c5bf9238b3f298
Matija Strlekar <mstrlekar@gmail.com>
2022-02-03 15:27:11 +0100

    Solidity basics v2

--- basics-v2/basics-v2.sol
+++ basics-v2/basics-v2.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 /// @title Contact title
 /// @author Matija
 /// @dev Comment for dev
-contract MyContract {
+/* contract MyContract {
     string public name = "Matija";
 
     // not public but can be used by smart contract and those who inherit it
@@ -49,10 +49,10 @@ contract MyContract {
         bool localPaused = true;
 
     }
-}
+} */
 
 // How to inherit stuff different contracts 
-contract iNeedVars is MyContract {
+/* contract iNeedVars is MyContract {
     uint256 public time2 = time;
 }
 
@@ -72,8 +72,7 @@ contract MyOperators {
 
         a++; // incrementing or -- decresing by one - changes assigment of "a"
 
-        b *= c; // same as b = b * c
- 
+        b *= c // same as b = b * c
     }
 
     function example_2() public view {
@@ -94,7 +93,6 @@ contract MyOperators {
         if(a != b) {
             result = true;
         }
-    }
 
 
     function example_3() public {
@@ -124,5 +122,125 @@ contract MyOperators {
     
         uint8 result = a < b ? 100 : 50; // turnary operator - same as JS
 
+    }
+}
+
+contract MyUnits {
+    //1 wei == 1;
+    //1 gewi == 1e9;
+    //1 ether == 1e18; 1000000000000000000 wei
+
+    // uint256 constOfNFT = 0.5; 
+    uint256 constOfNFT = 0.05 ether; // will be converted to wei
+    uint256 levelUpRate = 1 hours; // 60 min - beware of hours
+} */
+
+/* contract myIfElse {
+    uint256 public revealState = 0;
+    bool public isNowRevealed = false;
+    string public status = "Not yet";
+
+    function addToRevealState() public {
+        revealState++;
+    }
+
+    function isRevealed() public {
+
+        if(revealState >= 10) {
+
+            isNowRevealed = true;
+            status = "Reveleed";
+
+        } else {
+            status = "nope still not";
+        }
+
+    }
+} */
+
+/* contract MyLoop {
+
+    function myLoop() public pure {
+        for(uint256 i = 0; i < 5; i++) {
+
+            if(i == 3) {
+                break;
+            }
+
+            if(i == 2) {
+                continue;
+            }
+
+
+        }
+
+            uint256 j = 0;
+
+            while (j < 10) {
+
+        // do any actions
+
+
+
+            j++;
+
+    }
+    }
+
+} */
+
+/* contract MyMappings {
+
+// minting NFTs
+
+    mapping(uint256 => address) public nfts;
+
+    uint256 counter = 0;
+
+
+    function mintNFT(address _address) public {
+        nfts[counter] = _address;
+        counter++;
+    }
+
+
+} */
+
+/* contract MyArray {
+    uint256[] arr_1;
+    uint256[] arr_2 = [3,2,4,4,2,5,2,6,2];
+    uint256[5] arr_3;
+
+    // push new element to last 
+    // arr_2.push(9);
+
+    // remove last item in array
+    // arr_3.pop();
+
+    function getValueOfIndex(uint256 _index) public view returns (uint256) {
+        return arr_2[_index];
+    }
+
+    function addToArray(uint256 _value) public {
+        arr_2.push(_value);
+    }
+} */
+
+contract MyEnums {
+
+    enum Rarity {
+        original,
+        rare,
+        super_rare
+    }
+
+    Rarity public rarity;
+
+    constructor() {
+        rarity = Rarity.rare;
+    }
+
+    function makeSuperRare() public {
+       rarity = Rarity.super_rare;
     }
 }
